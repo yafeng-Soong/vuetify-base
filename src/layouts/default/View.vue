ba618e4a5328117adef2d906ec6ee15846419008
yafeng-Soong <397655952@qq.com>
2023-01-29 16:57:05 +0800

    调整布局

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,10 +1,28 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <v-main>
+  <v-app-bar class="px-3" color="white" flat density="compact">
+    <v-avatar color="grey-darken-1" size="32"></v-avatar>
+
+    <v-spacer></v-spacer>
+
+    <v-tabs centered color="grey-darken-2">
+      <v-tab v-for="link in links" :key="link">
+        {{ link }}
+      </v-tab>
+    </v-tabs>
+    <v-spacer></v-spacer>
+
+    <v-avatar
+      class="hidden-sm-and-down"
+      color="grey-darken-1"
+      size="32"
+    ></v-avatar>
+  </v-app-bar>
+  <v-main class="bg-grey-lighten-3">
     <router-view />
   </v-main>
 </template>
 
 <script setup>
-//
+const links = ['Dashboard', 'Messages', 'Profile', 'Updates']
 </script>
