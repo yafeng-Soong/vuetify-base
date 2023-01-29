c3479562af0486a2680584464b5b7ea371470c03
yafeng-Soong <397655952@qq.com>
2023-01-29 13:11:53 +0800

    first commit

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <v-app>
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+import DefaultView from './View.vue'
+</script>
---
+++ src/layouts/default/View.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <v-main>
+    <router-view />
+  </v-main>
+</template>
+
+<script setup>
+//
+</script>
